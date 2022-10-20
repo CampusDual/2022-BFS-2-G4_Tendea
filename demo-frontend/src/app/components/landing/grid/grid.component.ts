4e697f5965f926b1bb930959762f0d7338739fcf
Pablo <112854244+PabloPFL@users.noreply.github.com>
2022-10-20 14:27:55 +0200

    Responsive grid

--- demo-frontend/src/app/components/landing/grid/grid.component.ts
+++ demo-frontend/src/app/components/landing/grid/grid.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDataSource } from 'src/app/model/datasource/products.datasource';
+import { Product } from 'src/app/model/product';
+import { AnyField, AnyPageFilter, SortFilter } from 'src/app/model/rest/filter';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-grid',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
+  dataSource: ProductDataSource;
+  fields = ['name', 'price', 'discount'];
 
-  constructor() { }
+  products: Product[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.dataSource = new ProductDataSource(this.productService);
+    const pageFilter = new AnyPageFilter(
+      '',
+      this.fields.map((field) => new AnyField(field)),
+      0,
+      20,
+      'name'
+    );
+    this.dataSource.getProducts(pageFilter);
   }
+
+  // loadProductsPage() {
+  //   const pageFilter = new AnyPageFilter(
+  //     this.input.nativeElement.value,
+  //     this.fields.map((field) => new AnyField(field)),
+  //     this.paginator.pageIndex,
+  //     this.paginator.pageSize
+  //   );
+  //   pageFilter.order = [];
+  //   pageFilter.order.push(
+  //     new SortFilter(this.sort.active, this.sort.direction.toString())
+  //   );
+  //   this.dataSource.getProducts(pageFilter);
+  // }
 
 }
