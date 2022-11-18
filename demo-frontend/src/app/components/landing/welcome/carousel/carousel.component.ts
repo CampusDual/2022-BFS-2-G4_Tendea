3fc5dd3670ad26869390d0ca0cc3a57a4294a8f7
Roi <112854286+roigandara@users.noreply.github.com>
2022-11-18 13:10:51 +0100

    welcome fix

--- demo-frontend/src/app/components/landing/welcome/carousel/carousel.component.ts
+++ demo-frontend/src/app/components/landing/welcome/carousel/carousel.component.ts
@@ -3,32 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styles: [
-    `
-      .carousel-innerimg {
-        width: 960px;
-        max-height: 625px;
-      }
-      .carousel-caption {
-        bottom: 18rem;
-      }
-      h1 {
-        color: #00af91;
-      }
-      h3 {
-        color: white;
-      }
-      #entrance-button{
-        margin-right: 1vw;
-        margin-up: 20vw;
-        color: white;
-        font-size: 25px;
-        line-height: 18px;
-        height: 40px;
-        width: 300px;
-    }
-    `,
-  ],
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
   constructor() {}
--- demo-frontend/src/app/components/landing/welcome/welcome.component.ts
+++ demo-frontend/src/app/components/landing/welcome/welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styles: [],
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
   welcome: boolean = true;
