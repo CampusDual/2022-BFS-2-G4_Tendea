758a3bafe6936d2a22c1f92149bb86b5e2220ee4
Pablo <112854244+PabloPFL@users.noreply.github.com>
2022-11-18 13:39:21 +0100

    footer

--- demo-frontend/src/app/components/landing/footer/footer.component.ts
+++ demo-frontend/src/app/components/landing/footer/footer.component.ts
@@ -4,17 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styles: [
-    `
-      .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: red;
-        color: white;
-        text-align: center;
-      }
-    `,
+    // `
+    //   .footer {
+    //     position: fixed;
+    //     left: 0;
+    //     bottom: 0;
+    //     width: 100%;
+    //     background-color: red;
+    //     color: white;
+    //     text-align: center;
+    //   }
+    // `,
   ],
 })
 export class FooterComponent implements OnInit {
